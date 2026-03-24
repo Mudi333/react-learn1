90d50deff2f7d3de1f7987554c9dcb98ad05cb02
Muditha <muditha.jayasooriya@dci-student.org>
2026-03-24 19:24:36 +0100

    third commit

---
+++ vite1/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+
+function App() {
+
+const title = 'React'
+  return (
+    <>
+   <div><h1>revision of React</h1>
+   <h2>{title}</h2>
+   </div>
+    </>
+  )
+}
+
+export default App
